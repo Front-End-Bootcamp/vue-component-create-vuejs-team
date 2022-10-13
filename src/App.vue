5c53f77ab712c54564be1145b983da458eb0f5f9
enqinsel <enqinsel@gmail.com>
2022-10-13 23:04:21 +0300

    task created

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,50 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import Popup from './components/Popup.vue';
+
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <Popup class="v1"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+      header="Bu bir başlık"
+      srcImage="https://picsum.photos/200/300"
+      buttonText="textBlack" 
+      buttonColor="blue">
+    </Popup>
+
+    <Popup class="v2"
+      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries."
+      header="Başlık" 
+      srcImage="https://picsum.photos/200/300" 
+      srcVideo="" 
+      heightVideo="" 
+      widthVideo="" 
+      typeVideo=""
+      buttonText="textBlack" 
+      buttonColor="red">
+    </Popup>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
+.v1 {
+  width: 600px;
+  left: 350px;
 }
 
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
+.v2 {
+  width: 600px;
+  left: 1170px;
 }
 
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+.container {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgb(192, 192, 192);
 }
 </style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,125 @@
+<script setup>
+const props = defineProps([
+    "text",
+    "header",
+    "srcImage",
+    "srcVideo",
+    "heightVideo",
+    "widthVideo",
+    "typeVideo",
+    "buttonColor",
+    "buttonText"
+])
+</script>
+
+<template>
+    <div class="card">
+        <div class="card__label">
+            <h1 :header="props.header">{{header}}</h1>
+            <a href=""><img :src="props.srcImage"></a>
+        </div>
+        <div class="card__wrapper">
+            <p :text="props.text">{{text}}</p>
+            <!-- <video :widthVideo="props.widthVideo" :heightVideo="props.heightVideo" autoplay>
+                <source :srcVideo="props.srcVideo" :typeVideo="props.typeVideo">
+            </video> -->
+            <iframe width="560" height="115" src="https://www.youtube.com/embed/0qanF-91aJo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="card__footer">
+            <button class="button">Accept</button>
+            <button class="button" :class="[props.buttonColor, props.buttonText]">Close</button>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.card {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    z-index: 999;
+
+    &__label {
+        position: absolute;
+        width: 590px;
+        height: 300px;
+        display: flex;
+        align-items: stretch;
+        justify-content: space-around;
+        left: 50%;
+        top: 25%;
+        padding: 5px;
+        transform: translate(-50%, -50%);
+        background-color: antiquewhite;
+    }
+
+    &__wrapper {
+        position: absolute;
+        width: 590px;
+        height: 280px;
+        background-color: antiquewhite;
+        display: inline-block;
+        left: 50%;
+        top: 65%;
+        transform: translate(-50%, -50%);
+        padding: 5px;
+        overflow: hidden;
+        border-top: 1px dashed brown;
+        margin-top: 12px;
+    }
+
+    &__footer {
+        position: absolute;
+        width: 590px;
+        height: 40px;
+        background-color: antiquewhite;
+        border-top: 1px dashed brown;
+        left: 50%;
+        top: 86%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 5px;
+        margin-top: 30px;
+    }
+}
+
+.button {
+    // Default
+    background-color: #000000;
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+.red{
+    background-color: red;
+}
+
+.blue{
+    background-color: blue;
+}
+
+.green{
+    background-color: green;
+}
+
+.textBlack{
+    color: rgb(0, 0, 0);
+}
+
+.textWhite{
+    color: rgb(255, 255, 255);
+}
+</style>
