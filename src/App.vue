815420d29357f5543ab4f919f18884f35e53c028
enqinsel <enqinsel@gmail.com>
2022-10-18 22:50:26 +0300

    App and pupup updated

--- src/App.vue
+++ src/App.vue
@@ -1,199 +1,89 @@
 <script setup>
-import Popup from "./components/Popup.vue";
+import Popup from './components/Popup.vue';
+import { ref } from "vue"
+
+const showvideoImage = ref(false)
+const showtextTitle = ref(false)
+const showtitleImage = ref(false)
+const showtitletextImage = ref(false)
+
+const showvideoimageHandler = () =>{
+  showvideoImage.value = true
+}
+
+const showtexttitleHandler = () =>{
+  showtextTitle.value = true
+}
+
+const showtitleimageHandler = () => {
+  showtitleImage.value = true
+}
+
+const showtitletextimageHandler = () => {
+  showtitletextImage.value = true
+}
+
 </script>
 
+
 <template>
-	<div class="container">
-		<Popup
-			class="TTA"
-			text="default text"
-			header="Başlık"
-			buttonText="textWhite"
-			buttonColor="green"
-			:showClose="false"
-			:showAccept="true"
-		>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book.
-			</p>
-		</Popup>
+  <div class="container">
+     <Popup v-if="showtextTitle" class="TTA"
+      text="default"
+      header="Title"
+      buttonText="textWhite" 
+      buttonColor="green"
+      :showClose="false"
+      :showAccept="true">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </Popup>
 
-		<Popup
-			class="TIC"
-			header="Başlık"
-			buttonText="textBlack"
-			buttonColor="red"
-			:showClose="true"
-			:showAccept="false"
-		>
-			<img src="/vite.svg" />
-		</Popup>
+    <Popup v-if="showtitleImage" class="TIC"
+      header="Title"  
+      buttonText="textBlack" 
+      buttonColor="red"
+      :showClose="true"
+      :showAccept="false">
+      <img src="/vite.svg">
+    </Popup> 
 
-		<Popup
-			class="VTTC"
-			text="default text"
-			header="Başlık"
-			buttonText="textBlack"
-			buttonColor="red"
-			:showClose="true"
-			:showAccept="false"
-		>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. At, adipisci?
-			</p>
-			<video class="video" autoplay>
-				<source src="/punisher.mp4" type="video/mp4" />
-			</video>
-			<img src="/vite.svg" />
-		</Popup>
+    <Popup v-if="showvideoImage" class="VTTC"
+      text="default"
+      header="Title" 
+      buttonText="textBlack" 
+      buttonColor="red"
+      :showClose="true"
+      :showAccept="false">
 
-		<Popup
-			class="ITTC"
-			text="default"
-			header="Başlık"
-			buttonText="textBlack"
-			buttonColor="red"
-			:showClose="true"
-			:showAccept="false"
-		>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. At, adipisci?
-			</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, adipisci?</p>
+      <video class="video" controls autoplay>
+        <source src="/punisher.mp4" type="">
+      </video>
+      <img class="img" src="/vite.svg">
+    </Popup>
 
-			<img src="/vite.svg" />
-		</Popup>
+    <Popup v-if="showtitletextImage" class="ITTC"
+      text="default"
+      header="Title" 
+      buttonText="textBlack" 
+      buttonColor="red"
+      :showClose="true"
+      :showAccept="false">
 
-		<Popup
-			class="IA"
-			text=""
-			header=""
-			buttonText="textBlack"
-			buttonColor="green"
-			:showClose="false"
-			:showAccept="true"
-		>
-			<img src="https://picsum.photos/290/290" />
-		</Popup>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, adipisci?</p>
+      
+      <img src="/vite.svg">
+    </Popup>
 
-		<Popup
-			class="IAC"
-			text=""
-			header=""
-			buttonText="textBlack"
-			buttonColor="green"
-			:showClose="false"
-			:showAccept="true"
-		>
-			<img src="https://picsum.photos/290/290" />
-		</Popup>
 
-		<Popup
-			class="TTAC"
-			text=""
-			header="TİTLE"
-			buttonText="textBlack"
-			buttonColor="red"
-			:showClose="true"
-			:showAccept="true"
-		>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-				facilis? Ex, qui? Minima dolorum quisquam quas quod iste, nisi nemo
-				rerum vitae ipsa illum quaerat tenetur temporibus amet deserunt fugit!
-			</p>
-			<img src="" />
-		</Popup>
+    <button @click="showvideoimageHandler">Show Video+Image+Text Popup</button>
+    <button @click="showtexttitleHandler">Show Text+Title Popup</button>
+    <button @click="showtitleimageHandler">Show Title+Image Popup</button>
+    <button @click="showtitletextimageHandler">Show Title+Text+Image</button>
 
-		<Popup
-			class="TTC"
-			text=""
-			header="TİTLE"
-			buttonText="textBlack"
-			buttonColor="red"
-			:showClose="true"
-			:showAccept="false"
-		>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-				facilis? Ex, qui? Minima dolorum quisquam quas quod iste, nisi nemo
-				rerum vitae ipsa illum quaerat tenetur temporibus amet deserunt fugit!
-			</p>
-			<img src="" />
-		</Popup>
-	</div>
+  </div>
+
+
 </template>
 
-<style scoped>
-.video {
-	width: 70%;
-	height: 70%;
-}
 
-.container {
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
-	background-color: rgb(192, 192, 192);
-	
-}
-
-.container::-webkit-scrollbar{
-	background: #eee;
-	width: 12px;
-	height: 12px;
-	border-radius: 4px 4px;
-}
-
-.container::-webkit-scrollbar-corner {
-	background: #eee;
-
-}
-
-.container::-webkit-scrollbar-thumb{
-	background: red;
-	border-radius: 6px;
-}
-.VTTC {
-	margin: auto;
-	left: 1000px;
-}
-.ITTC {
-	margin: auto;
-	left: 600px;
-}
-.TTA {
-	margin: auto;
-	left: 180px;
-}
-
-.TIC {
-	margin: auto;
-	left: 1360px;
-}
-
-.IA {
-	top: 90%;
-	left: 180px;
-}
-
-.IAC {
-	top: 90%;
-	left: 600px;
-}
-
-.TTAC {
-	top: 90%;
-	left: 1000px;
-}
-
-.TTC {
-	top: 90%;
-	left: 1360px;
-}
-</style>
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,122 +1,45 @@
 <script setup>
+import { ref } from "vue"
+
+const close = ref(true)
 
 const props = defineProps([
-    "text",
-    "header",
-    "buttonColor",
-    "buttonText",
-    "showClose",
-    "showAccept"
+	"text",
+	"header",
+	"buttonColor",
+	"buttonText",
+	"showClose",
+	"showAccept"
 ])
+
+
+const closeHandler = () => {
+	close.value = false
+}
 
 </script>
 
 <template>
-    <div class="card">
-        <div class="card__label">
-            <h1 :header="props.header">{{header}}</h1>
-        </div>
-        <div class="card__wrapper">
-					<!-- Text , Image , Video vb. eklemeler yapilabilir. -->
-            <slot>
-                {{props.text}}
-            </slot>
-        </div>
-        <div class="card__footer">
-            <button v-if="props.showAccept" class="button" :class="[props.buttonColor, props.buttonText]">Accept</button>
-            <button v-if="props.showClose" class="button" :class="[props.buttonColor, props.buttonText]">Close</button>
-        </div>
-    </div>
+	<div class="mask" v-if="close"></div>
+	<div class="card" v-if="close">
+
+		<div class="card__label">
+			<h1 :header="props.header">{{header}}</h1>
+		</div>
+		<div class="card__wrapper">
+			<slot>
+				{{props.text}}
+			</slot>
+		</div>
+		<div class="card__footer">
+			<button v-if="props.showAccept" @click="closeHandler" class="button"
+				:class="[props.buttonColor, props.buttonText]">Accept</button>
+			<button v-if="props.showClose" @click="closeHandler" class="button"
+				:class="[props.buttonColor, props.buttonText]">Close</button>
+		</div>
+
+	</div>
+
 </template>
 
 
-<style lang="scss" scoped>
-.card {
-	margin: auto;
-	flex: auto;
-    position: fixed;
-    top: 25%;
-    transform: translate(-50%, -50%);
-    width: 100%;
-    height: 100%;
-    z-index: 999;
-
-    &__label {
-        position: absolute;
-        width: 290px;
-        height: 100px;
-        display: flex;
-        align-items: stretch;
-        justify-content: space-around;
-        left: 50%;
-        top: 40%;
-        padding: 5px;
-        transform: translate(-50%, -50%);
-        background-color: antiquewhite;
-    }
-
-    &__wrapper {
-        position: absolute;
-        width: 290px;
-        height: 280px;
-        background-color: antiquewhite;
-        display: inline-block;
-        left: 50%;
-        top: 65%;
-        transform: translate(-50%, -50%);
-        padding: 5px;
-        
-        border-top: 1px dashed brown;
-        margin-top: 12px;
-    }
-
-    &__footer {
-        position: absolute;
-        width: 290px;
-        height: 40px;
-        background-color: antiquewhite;
-        border-top: 1px dashed brown;
-        left: 50%;
-        top: 86%;
-        transform: translate(-50%, -50%);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 5px;
-        margin-top: 30px;
-    }
-}
-.button {
-    // Default
-    background-color: #000000;
-    border: none;
-    color: white;
-    padding: 10px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-}
-
-.red {
-    background-color: red;
-}
-
-.blue {
-    background-color: blue;
-}
-
-.green {
-    background-color: green;
-}
-
-.textBlack {
-    color: rgb(0, 0, 0);
-}
-
-.textWhite {
-    color: rgb(255, 255, 255);
-}
-</style>
