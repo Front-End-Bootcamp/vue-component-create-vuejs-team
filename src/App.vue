600f6b6fb791a5995906463998a6bf5350c28ff3
İsa Sarı <99514353+devisasari@users.noreply.github.com>
2022-10-18 23:06:47 +0300

    update App style

--- src/App.vue
+++ src/App.vue
@@ -86,4 +86,71 @@ const showtitletextimageHandler = () => {
 
 </template>
 
+<style scoped>
+.img{
+  width: 50px;
+}
+.video {
+    width: 300px;
+    height: 200px;
+ }
+
+button {
+  background-color: #acd01e;
+  border: 0 solid #E5E7EB;
+  box-sizing: border-box;
+  color: #000000;
+  display: flex;
+  font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 700;
+  justify-content: center;
+  line-height: 1.75rem;
+  padding: .75rem 1.65rem;
+  position: relative;
+  text-align: center;
+  text-decoration: none #000000 solid;
+  width: 100%;
+  max-width: 460px;
+  cursor: pointer;
+  transform: rotate(-2deg);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+    top: 40px;
+}
+
+button:focus {
+  outline: 0;
+    color: #ffffff;
+}
+
+button:after {
+  content: '';
+  position: absolute;
+  border: 1px solid #000000;
+  bottom: 4px;
+  left: 4px;
+  width: calc(100% - 1px);
+  height: calc(100% - 1px);
+}
+
+button:hover:after {
+  bottom: 2px;
+  left: 2px;
+}
+
+button:hover{
+    transform: translate(30px, -20px);
+    transition: all 0.3s ease;
+}
+
+@media (min-width: 768px) {
+  button {
+    padding: .75rem 3rem;
+    font-size: 1.25rem;
+  }
+}
+</style>
+
 
